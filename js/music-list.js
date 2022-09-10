7cc1e89cf0b0ecb12174729fd43428e6e46f9233
Subodh <suboodhsalve2001@gmail.com>
2022-09-10 15:11:03 +0530

    ok

---
+++ js/music-list.js
@@ -0,0 +1,40 @@
+let allMusic = [
+
+    {
+        name: "Butter",
+        artist: "BTS",
+        img: "butter",
+        src: "butter"
+    },
+    {
+        name: "Dynamite",
+        artist: "BTS",
+        img: "bts",
+        src: "dynamite"
+    },
+    {
+        name: "Rockstar",
+        artist: "Post Malone",
+        img: "rokstar",
+        src: "rockstar"
+    },
+    {
+        name: "Permission To Dance",
+        artist: "BTS",
+        img: "img-4",
+        src: "permission"
+    },
+    {
+        name: "LA LA LA",
+        artist: "Shakira",
+        img: "shakira",
+        src: "lala"
+    },
+    {
+        name: "Baby",
+        artist: "Justin Bieber",
+        img: "img-6",
+        src: "baby"
+    },
+    
+    ];
